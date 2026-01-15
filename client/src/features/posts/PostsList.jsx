680130ab5bb6f1c771ea7f4fc2357dd1ffa085cb
Jason Tunstill <tunstij@gmail.com>
2026-01-14 21:38:43 -0600

    Added Vite REact client app. Fixed CORS

---
+++ client/src/features/posts/PostsList.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { API_URL } from '../../constants';
+
+function PostsList() {
+  const [posts, setPosts] = useState([]);
+  const [, setLoading] = useState(true);
+  const [, setError] = useState(null);
+
+  // Fetch posts from API
+    useEffect(() => {
+      async function loadPosts() {
+        try {
+          // const response = await fetch(API_URL);
+          const response = await fetch("http://localhost:3000/api/v1/posts");
+          if(response.ok) {
+            const json = await response.json();
+            setPosts(json);
+          }
+          else {
+            throw response;
+          }
+        }
+        catch (e) {
+          const msg = "An unknown error has occurred ...";
+          setError(msg);
+          console.log(msg, e);
+        }
+        finally {
+          setLoading(false);
+        }
+      }
+      loadPosts();
+    }, []);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id} className="post-container">
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default PostsList;
